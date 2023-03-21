6bc5a13b632cafae3c19ab3045d58a1b2e6b97be
Jason Davenport <jasondavenport@google.com>
2023-03-21 15:41:24 -0600

    more linting fixes

--- versions.tf
+++ versions.tf
@@ -2,11 +2,19 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.52.0"
+      version = "~> 4.56"
     }
     random = {
       source  = "hashicorp/random"
       version = ">= 2"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = ">= 2"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.9.1"
     }
   }
   required_version = ">= 0.13"
