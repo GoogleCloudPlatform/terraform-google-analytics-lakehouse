8a4a72b719d63323b669874f482c95a6997998d9
“Steve <steveswalker@google.com>
2023-03-24 17:20:11 -0400

    remove run-workflow

--- main.tf
+++ main.tf
@@ -123,7 +123,7 @@ resource "google_storage_bucket_object" "pyspark_file" {
 #we will use this as a wait and to make sure every other resource in this project has completed.
 #we will then make the last four workflow steps dependent on this
 resource "time_sleep" "wait_after_all_resources" {
-  create_duration = "30s"
+  create_duration = "120s"
   depends_on = [
     module.project-services,
     google_storage_bucket.provisioning_bucket,
@@ -149,7 +149,7 @@ resource "time_sleep" "wait_after_all_resources" {
 }
 
 resource "time_sleep" "wait_after_all_workflows" {
-  create_duration = "30s"
+  create_duration = "60s"
   depends_on = [data.http.call_workflows_bucket_copy_run,
   data.http.call_workflows_create_gcp_biglake_tables_run,
   data.http.call_workflows_create_iceberg_table,
@@ -161,49 +161,18 @@ data "google_client_config" "current" {
 }
 provider "http" {
 }
-data "http" "call_workflows_create_gcp_biglake_tables_run" {
-  url = "https://workflowexecutions.googleapis.com/v1/projects/${module.project-services.project_id}/locations/${var.region}/workflows/${google_workflows_workflow.workflows_create_gcp_biglake_tables.name}/executions"
+
+data "http" "call_workflows_bucket_copy_run" {
+  url = "https://workflowexecutions.googleapis.com/v1/projects/${module.project-services.project_id}/locations/${var.region}/workflows/${google_workflows_workflow.workflow_bucket_copy.name}/executions"
   method = "POST"
   request_headers = {
     Accept = "application/json"
   Authorization = "Bearer ${data.google_client_config.current.access_token}" }
-  depends_on = [
-    module.project-services,
-    google_storage_bucket.provisioning_bucket,
-    google_storage_bucket.destination_bucket,
-    google_project_service_identity.workflows,
-    google_service_account.workflows_sa,
-    google_project_iam_member.workflow_service_account_invoke_role,
-    google_project_iam_member.workflows_sa_bq_data,
-    google_project_iam_member.workflows_sa_gcs_admin,
-    google_project_iam_member.workflows_sa_bq_resource_mgr,
-    google_project_iam_member.workflow_service_account_token_role,
-    google_project_iam_member.workflows_sa_bq_connection,
-    google_project_iam_member.workflows_sa_bq_read,
-    google_project_iam_member.workflows_sa_log_writer,
-    google_project_iam_member.workflow_service_account_dataproc_role,
-    google_project_iam_member.workflow_service_account_bqadmin,
-    google_bigquery_dataset.gcp_lakehouse_ds,
-    google_bigquery_connection.gcp_lakehouse_connection,
-    google_project_iam_member.connectionPermissionGrant,
-    google_workflows_workflow.workflows_create_gcp_biglake_tables,
-    data.google_storage_project_service_account.gcs_account
-  ]  
+    depends_on = [
+    time_sleep.wait_after_all_resources
+  ]
 }
 
-resource "time_sleep" "wait_after_all_workflows" {
-  create_duration = "30s"
-  depends_on = [data.http.call_workflows_bucket_copy_run,
-  data.http.call_workflows_create_gcp_biglake_tables_run,
-  data.http.call_workflows_create_iceberg_table,
-  data.http.call_workflows_create_views_and_others
-  ]  
-}
-#execute workflows
-data "google_client_config" "current" {
-}
-provider "http" {
-}
 data "http" "call_workflows_create_gcp_biglake_tables_run" {
   url = "https://workflowexecutions.googleapis.com/v1/projects/${module.project-services.project_id}/locations/${var.region}/workflows/${google_workflows_workflow.workflows_create_gcp_biglake_tables.name}/executions"
   method = "POST"
