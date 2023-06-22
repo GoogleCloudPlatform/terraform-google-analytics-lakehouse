629f00b679faf1f29c676514f0ef7869c7b9ee8a
Don McCasland <donmccasland@google.com>
2023-06-22 12:17:23 -0700

    fix: update neos toc url (#47)

--- outputs.tf
+++ outputs.tf
@@ -30,7 +30,7 @@ output "bigquery_editor_url" {
 }
 
 output "neos_tutorial_url" {
-  value       = "https://console.cloud.google.com/products/solutions/deployments?walkthrough_id=panels--sic--analytics-lakehouse_toc&project=${var.project_id}"
+  value       = "http://console.cloud.google.com/products/solutions/deployments?walkthrough_id=panels--sic--analytics-lakehouse_toc"
   description = "The URL to launch the in-console tutorial for the Analytics Lakehouse solution"
 }
 
