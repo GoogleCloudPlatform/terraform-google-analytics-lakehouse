ac77ec0e36f9e58e5281e776f7ac83d9132bded3
Hyunuk Lim <hyunuklim@google.com>
2023-10-25 22:41:22 -0700

    fix: lint

--- setup.sh
+++ setup.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 set -eu
 
@@ -21,13 +35,13 @@ else
     echo "Usage: $0 project_id=string region=string enable_apis=boolean force_destroy=boolean deletion_protection=boolean"
     exit 1
   fi
-  
+
   # Define an array of expected keys
   EXPECTED_KEYS=("project_id" "region" "enable_apis" "force_destroy" "deletion_protection")
 
   # Check if arguments have the correct format and keys
   for arg in "$@"; do
-    IFS="=" read -r key value <<< "$arg"    
+    IFS="=" read -r key value <<< "$arg"
     
     if [[ ! " ${EXPECTED_KEYS[@]} " =~ " $key " || ! "$arg" =~ ^[a-zA-Z_][a-zA-Z0-9_]*=.*$ ]]; then
       echo "Arguments must be in the format: key=value and the key must be either of 'project_id', 'region', 'enable_apis', 'force_destroy', 'deletion_protection'"
