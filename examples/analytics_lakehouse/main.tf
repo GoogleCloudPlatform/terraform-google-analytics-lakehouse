12527436103be4e3ad1eba2cbc48350470755434
Bharath KKB <bharathkrishnakb@gmail.com>
2023-12-21 14:59:58 -0600

    Update main.tf

--- examples/analytics_lakehouse/main.tf
+++ examples/analytics_lakehouse/main.tf
@@ -16,7 +16,7 @@
 
 module "analytics_lakehouse" {
   source  = "GoogleCloudPlatform/analytics-lakehouse/google"
-  version = "~> REPLACE-AFTER-RELEASE"
+  version = "~> 0.3"
 
   project_id    = var.project_id
   region        = "us-central1"
