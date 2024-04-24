f9ec07a4f4b2470612024d72f930546d25f97eb8
Brad Miro <bmiro@google.com>
2024-04-24 11:13:27 -0400

    updateS

--- dataplex.tf
+++ dataplex.tf
@@ -184,7 +184,6 @@ resource "google_dataplex_asset" "gcp_primary_tables" {
 # Discovery on this data generally takes 6-8 minutes.
 resource "time_sleep" "wait_for_dataplex_discovery" {
   depends_on = [
-    google_dataplex_asset.gcp_primary_curated_bi,
     google_dataplex_asset.gcp_primary_tables,
     google_dataplex_asset.gcp_primary_ga4_obfuscated_sample_ecommerce,
     google_dataplex_asset.gcp_primary_textocr
